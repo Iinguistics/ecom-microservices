afb6cd0c56b988e204ab10e2311363ad3646ada7
James <Thylinguistics@gmail.com>
2024-08-31 16:43:12 -0700

    adds db construct & product table

---
+++ lib/database.ts
@@ -0,0 +1,31 @@
+import { Construct } from 'constructs';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import {
+	AttributeType,
+	BillingMode,
+	ITable,
+	Table,
+} from 'aws-cdk-lib/aws-dynamodb';
+
+export class Database extends Construct {
+	public readonly productTable: ITable;
+
+	constructor(scope: Construct, id: string) {
+		super(scope, id);
+
+		this.productTable = this.createProductTable();
+	}
+
+	private createProductTable(): ITable {
+		const productTable = new Table(this, 'product', {
+			partitionKey: {
+				name: 'id',
+				type: AttributeType.STRING,
+			},
+			tableName: 'product',
+			removalPolicy: RemovalPolicy.DESTROY,
+			billingMode: BillingMode.PAY_PER_REQUEST,
+		});
+		return productTable;
+	}
+}
