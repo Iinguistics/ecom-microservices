ca8feed32b2b7ee6113cc65946064f529f818f4e
James <Thylinguistics@gmail.com>
2024-09-01 14:33:46 -0700

    adds product creation

--- src/product/Service/ProductTable.ts
+++ src/product/Service/ProductTable.ts
@@ -20,7 +20,7 @@ class ProductTable {
 		console.log('fetching all products');
 		try {
 			const params = {
-				TableName: process.env.DYNAMODB_TABLE_NAME,
+				TableName: process.env.DYNAMODB_TABLE_NAME!,
 			};
 
 			const data = await ddbClient.send(new ScanCommand(params));
@@ -36,7 +36,7 @@ class ProductTable {
 
 		try {
 			const params = {
-				TableName: process.env.DYNAMODB_TABLE_NAME,
+				TableName: process.env.DYNAMODB_TABLE_NAME!,
 				Key: marshall({ id: productId }),
 			};
 
@@ -69,13 +69,41 @@ class ProductTable {
 				throw new Error('Invalid request: Request body is empty');
 			}
 
-			const params = this.buildUpdateParams(event.pathParameters.id, requestBody, objKeys);
+			const params = this.buildUpdateParams(
+				event.pathParameters.id,
+				requestBody,
+				objKeys
+			);
 
 			const updateResult = await ddbClient.send(new UpdateItemCommand(params));
 
 			return updateResult;
 		} catch (e) {
 			console.error(e);
+			return handleError(e);
+		}
+	}
+
+	// TODO: check for body fields / validation
+	async createProduct(event: APIGatewayProxyEventV2) {
+		if (!event.body) {
+			throw new Error(`Invalid request, missing event body`);
+		}
+
+		console.log('creating product');
+		try {
+			const productRequest = JSON.parse(event.body);
+			productRequest.id = uuidv4();
+
+			const params = {
+				TableName: process.env.DYNAMODB_TABLE_NAME!,
+				Item: marshall(productRequest),
+			};
+
+			const createResult = await ddbClient.send(new PutItemCommand(params));
+
+			return createResult;
+		} catch (e) {
 			return handleError(e);
 		}
 	}
