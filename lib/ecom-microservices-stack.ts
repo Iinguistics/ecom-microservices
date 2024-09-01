6bfcba557c61fc88fff44d48369d17415a1287c0
James <Thylinguistics@gmail.com>
2024-08-31 18:04:59 -0700

    connects constructs

--- lib/ecom-microservices-stack.ts
+++ lib/ecom-microservices-stack.ts
@@ -1,12 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
+import { ApiGateway } from './apigateway';
 import { Construct } from 'constructs';
 import { Database } from './database';
+import { Microservices } from './microservice';
 
 export class EcomMicroservicesStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    const database = new Database(this, 'Database')
+		const database = new Database(this, 'Database');
 
-  }
+		const microservices = new Microservices(this, 'Microservices', {
+			productTable: database.productTable,
+		});
+
+		const apigateway = new ApiGateway(this, 'ApiGateway', {
+			productMicroservice: microservices.productMicroservice,
+		});
+	}
 }
