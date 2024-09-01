192a15bd489def34a43e767c629c6c84b8cb769e
James <Thylinguistics@gmail.com>
2024-08-31 17:19:37 -0700

    adds api gateway

---
+++ lib/apigateway.ts
@@ -0,0 +1,32 @@
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+
+interface ApiGatewayProps {
+	productMicroservice: IFunction;
+}
+
+export class ApiGateway extends Construct {
+	constructor(scope: Construct, id: string, props: ApiGatewayProps) {
+		super(scope, id);
+
+		this.createProductApi(props.productMicroservice);
+	}
+
+	private createProductApi(productMicroservice: IFunction) {
+		const apigw = new LambdaRestApi(this, 'productApi', {
+			restApiName: 'Product Service',
+			handler: productMicroservice,
+			proxy: false,
+		});
+
+		const product = apigw.root.addResource('product');
+		product.addMethod('GET'); // GET /product
+		product.addMethod('POST'); // POST /product
+
+		const singleProduct = product.addResource('{id}'); // product/{id}
+		singleProduct.addMethod('GET'); // GET /product/{id}
+		singleProduct.addMethod('PUT'); // PUT /product/{id}
+		singleProduct.addMethod('DELETE'); // DELETE /product/{id}
+	}
+}
